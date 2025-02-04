56f1a97492f41b54e9f5d7e704bf28ca97783a98
Abhinav Sharma <abhinavs@iprdgroup.com>
2025-02-04 19:02:49 +0530

    added constants required as per development roadmap

---
+++ constants/itemType.ts
@@ -0,0 +1,24 @@
+// add all of thiese "string" | "boolean" | "group" | "display" | "decimal" | "date" | "dateTime" | "time" | "text" | "integer" | "choice" | "open-choice" | "attachment" | "quantity" | "question" | "url" | "reference"' is not assignable to parameter of type '"string" | "boolean" | "group" | "display" | "decimal" | "date" | "dateTime" | "time" | "text" | "integer" | "choice" | "open-choice" | "attachment" | "quantity"
+const ItemType = {
+  BOOLEAN: "boolean",
+  GROUP: "group",
+  DISPLAY: "display",
+  DECIMAL: "decimal",
+  DATE: "date",
+  DATETIME: "dateTime",
+  TIME: "time",
+  TEXT: "text",
+  STRING: "string",
+  INTEGER: "integer",
+  CHOICE: "choice",
+  OPENCHOICE: "open-choice",
+  ATTATCHMENT: "attachment",
+  QUANTITY: "quantity",
+  REFERENCE: "reference",
+  URL: "url",
+  QUESTION: "question",
+} as const
+
+export default ItemType
+
+export type IItemType = (typeof ItemType)[keyof typeof ItemType]
